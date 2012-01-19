3869b1b1c41b9f6c7928dc5496339b356a09c1f6
Steve Weis <steveweis@gmail.com>
2012-01-19 14:36:08 -0800

    Tweaking pom and PublicKey class

--- src/main/java/crest/PublicKey.java
+++ src/main/java/crest/PublicKey.java
@@ -1,12 +1,38 @@
 package crest;
 
+import java.util.Date;
+
+/**
+ * Hibernate entity for public keys
+ */
 public class PublicKey {
   private Long id;
   private String keyValue;
+  private Date timestamp;
   
-  public PublicKey() {
-    
+  public PublicKey() {  }
+
+  private Long getId() {
+    return id;
   }
-  
-  
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getKeyValue() {
+    return keyValue;
+  }
+
+  public void setKeyValue(String keyValue) {
+    this.keyValue = keyValue;
+  }
+
+  public Date getTimestamp() {
+    return timestamp;
+  }
+
+  public void setTimestamp(Date timestamp) {
+    this.timestamp = timestamp;
+  }
 }
