fd3adf37ff241eb5e747c7c0d3fff126d8e99c76
Steve Weis <steveweis@gmail.com>
2012-01-26 16:48:45 -0800

    Cleaning up code.

--- src/main/java/crest/PublicKey.java
+++ src/main/java/crest/PublicKey.java
@@ -1,5 +1,6 @@
 package crest;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.Serializable;
@@ -15,6 +16,10 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import org.apache.commons.codec.binary.Hex;
+
+import com.sun.jersey.core.util.Base64;
+
 /**
  * Hibernate entity for public keys
  */
@@ -22,6 +27,8 @@ import javax.persistence.Table;
 @Table(name = "public_keys")
 public class PublicKey implements Serializable {
   private static final long serialVersionUID = -4175765959576175717L;
+  private static final String PEM_HEADER = "-----BEGIN RSA PUBLIC KEY-----";
+  private static final String PEM_FOOTER = "-----END RSA PUBLIC KEY-----";
   private byte[] keyValue;
   private String keyHash;
   private Date createdOn;
@@ -29,19 +36,25 @@ public class PublicKey implements Serializable {
   public PublicKey() {
     // Empty constructor for Hibernate
   }
-  
+
   public PublicKey(InputStream x509Stream) throws GeneralSecurityException, IOException {
-    byte[] x509Data = Util.readStreamFully(x509Stream);
+    ByteArrayOutputStream tempStream = new ByteArrayOutputStream();
+    byte[] buf = new byte[8192];
+    int bytesRead = 0;
+    while ((bytesRead = x509Stream.read(buf)) != -1) {
+      tempStream.write(buf, 0, bytesRead);
+    }
+    byte[] x509Data = tempStream.toByteArray();
     X509EncodedKeySpec keySpec = new X509EncodedKeySpec(x509Data);
 
-    // This just tests that the public key parses correctly
+    // This will throw an exception if the key spec is not a valid form 
     RSAPublicKey javaPublicKey = (RSAPublicKey) 
         KeyFactory.getInstance("RSA").generatePublic(keySpec);    
     if (javaPublicKey != null) {
       setCreatedOn(new Date());
       setKeyValue(x509Data);
       MessageDigest sha1 = MessageDigest.getInstance("SHA-1");
-      setKeyHash(Util.byteArrayToHex(sha1.digest(x509Data)));
+      setKeyHash(new String(Hex.encodeHex(sha1.digest(x509Data), true)));
     }
   }
 
@@ -71,5 +84,10 @@ public class PublicKey implements Serializable {
 
   public void setCreatedOn(Date createdOn) {
     this.createdOn = createdOn;
+  }
+  
+  public String toString() {
+    String encoded = new String(Base64.encode(getKeyValue()));
+    return String.format("%s\n%s\n%s\n", PEM_HEADER, encoded, PEM_FOOTER);
   }
 }
--- src/main/java/crest/RestHandler.java
+++ src/main/java/crest/RestHandler.java
@@ -4,47 +4,58 @@ import java.io.IOException;
 import java.security.GeneralSecurityException;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
-import org.hibernate.Criteria;
-import org.hibernate.Session;
-import org.hibernate.criterion.Projections;
-import org.hibernate.criterion.Restrictions;
-
 @Path("/v1")
 public class RestHandler {
+  
+  private final GenericDao<PublicKey, String> publicKeyDao =
+      new GenericDao<PublicKey, String>(PublicKey.class, HibernateUtil.getSessionFactory());
+  
   @PUT
   @Path("/publickey")
   @Produces("text/plain")
-  public Response testDatabaseWrite(@Context HttpServletRequest request) {
+  public Response putPublicKey(@Context HttpServletRequest request) {
     if (request.getContentLength() == 0) {
       return Response.status(Status.BAD_REQUEST).entity("No data").build();
-    }
-
+    }    
+    PublicKey pubKey = null;
     try {
-      PublicKey pubKey = new PublicKey(request.getInputStream());      
-      Session session = HibernateUtil.getSessionFactory().getCurrentSession();
-      session.beginTransaction();
-      Criteria criteria = session.createCriteria(PublicKey.class)
-          .add(Restrictions.eq("keyHash", pubKey.getKeyHash()))
-          .setProjection(Projections.id());
-      String message = String.format("Public key already exists: %s", pubKey.getKeyHash());
-      if (criteria.uniqueResult() == null) {
-        // If the key does not already exist, then store it
-        session.save(pubKey);
-        message = String.format("Stored public key: %s", pubKey.getKeyHash());
-      }
-      session.getTransaction().commit();
-      return Response.status(Status.OK).entity(message).build();          
+      pubKey = new PublicKey(request.getInputStream());
+    } catch (GeneralSecurityException e) {
+      return Response.status(Status.BAD_REQUEST).entity(e.getMessage()).build();
     } catch (IOException e) {
       return Response.status(Status.BAD_REQUEST).entity(e.getMessage()).build();
-    } catch (GeneralSecurityException e) {
-      return Response.status(Status.BAD_REQUEST).entity(e.getMessage()).build();  
+    }
+    // The public key was properly initialized from the request input stream
+    PublicKey existingKey = publicKeyDao.findById(pubKey.getKeyHash());
+ 
+    String message = String.format("Public key already exists: %s\n", pubKey.getKeyHash());
+    if (existingKey == null) {
+      // If the key does not already exist, then store it
+      String keyHash = publicKeyDao.save(pubKey);
+      message = String.format("Stored public key: %s\n", keyHash);
+    }
+    return Response.status(Status.OK).entity(message).build();      
+  }
+  
+  @GET
+  @Path("/publickey/{keyHash}")
+  @Produces("text/plain")
+  public Response getPublicKey(@PathParam("keyHash") String keyHash) {
+    PublicKey existingKey = publicKeyDao.findById(keyHash);
+    if (existingKey == null) {
+      return Response.status(Status.NOT_FOUND).entity(
+          String.format("Key %s not found\n", keyHash)).build();      
+    } else {
+      return Response.status(Status.OK).entity(existingKey.toString()).build();
     }
   }
 }
